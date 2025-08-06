3a1eb5360458873ecb4b465c5be60c952559928f
Lazagga <81786003+Lazagga@users.noreply.github.com>
2025-08-06 14:25:50 +0900

    feat: add Menu in UIManager

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [Header("PlayerInfo")]
+    public Slider HP_bar;
+    public Slider MP_bar;
+
+    [Header("Menu")]
+    public GameObject Menu;
+    public Slider BGM_Volume;
+    public Slider SE_Volume;
+    public Button QuitButton;
+
+    public PlayerInput playerInput;
+
+    private void Awake()
+    {
+        if(null == Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
+
+        Menu.SetActive(false);
+
+        InputActionMap inputs = playerInput.actions.FindActionMap("UI");
+        inputs.FindAction("ESC").performed += ToggleMenu;
+
+        QuitButton.onClick.AddListener(QuitGame);
+    }
+
+    public void ToggleMenu(InputAction.CallbackContext context)
+    {
+        Menu.SetActive(Menu.activeSelf ? false : true);
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
