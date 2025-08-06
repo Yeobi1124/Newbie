044424937512769b7a389dfe126db6310542ccb5
Yeobi1124 <yeobi1124@gmail.com>
2025-08-06 15:20:18 +0900

    feat: Add Heavy Attack

---
+++ Assets/Scripts/Skill/HeavyAttack.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class HeavyAttack : Skill
+{
+    [SerializeField] private GameObject createPoint;
+    [SerializeField] private float _bulletSpeed = 5f;
+    public override void Use()
+    {
+        if (_energy.Energy < _energyConsumption) return;
+        
+        _energy.Energy -= _energyConsumption;
+        
+        GameObject bullet = ObjectManager.instance.PullObject("DroneShooter");
+        bullet.GetComponent<Attack>().damage = _damage;
+        
+        bullet.transform.position = createPoint.transform.position;
+        bullet.GetComponent<Rigidbody2D>().linearVelocityX = _bulletSpeed;
+    }
+}
