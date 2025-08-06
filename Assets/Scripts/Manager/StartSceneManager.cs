f938f8dd1791f365df38a30a012a36012e867b20
Lazagga <81786003+Lazagga@users.noreply.github.com>
2025-08-06 21:44:49 +0900

    feat: add arts and UI develop

--- Assets/Scripts/Manager/StartSceneManager.cs
+++ Assets/Scripts/Manager/StartSceneManager.cs
@@ -1,5 +1,7 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class StartSceneManager : MonoBehaviour
 {
@@ -7,11 +9,27 @@ public class StartSceneManager : MonoBehaviour
     public Button SettingButton;
     public Button QuitButton;
 
+    public TMP_Text time;
+
+    public float speed;
+
+    public GameObject[] BG = new GameObject[2];
     private void Awake()
     {
         PlayButton.onClick.AddListener(Play);
         SettingButton.onClick.AddListener(Setting);
         QuitButton.onClick.AddListener(Quit);
+    }
+
+    private void Update()
+    {
+        time.text = DateTime.Now.ToString("HH:mm:ss");
+
+        foreach(GameObject go in BG)
+        {
+            go.transform.Translate(-speed * Time.deltaTime, 0, 0);
+            if (go.transform.position.x < -22) go.transform.Translate(45, 0, 0);
+        }
     }
 
     public void Play()
