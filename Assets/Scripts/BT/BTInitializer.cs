eefa1a6c5825dad255b86bde51fb8f615322d8be
김창진 <kcjlazer@naver.com>
2025-08-07 04:42:59 +0900

    feat: 보스 레이저 공격 구현
    보스 레이저 구현

--- Assets/Scripts/BT/BTInitializer.cs
+++ Assets/Scripts/BT/BTInitializer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Behavior;
+using System.Collections.Generic;
 
 public class BTInitializer : MonoBehaviour
 {
@@ -8,14 +9,17 @@ public class BTInitializer : MonoBehaviour
     [SerializeField] private IMissileMover mover;
     [SerializeField] private Transform missileAbove;
     [SerializeField] private Transform missileBelow;
+    [SerializeField] private List<GameObject> backgrounds;
+    [SerializeField] private GameObject laser;
     void Start()
     {
         behaviorTree = GetComponent<BehaviorGraphAgent>();
         behaviorTree.SetVariableValue("Player", player);
         behaviorTree.SetVariableValue("MissilePosition_Above", missileAbove);
         behaviorTree.SetVariableValue("MissilePosition_Below", missileBelow);
-        //mover ³Ö±â
-    }
+		behaviorTree.SetVariableValue("Backgrounds", backgrounds);
+		behaviorTree.SetVariableValue("Laser", laser);
+	}
     void Update()
     {
         
