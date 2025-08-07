493d64dfd53e025f93b56f36a3548a94b3344c89
Yeobi1124 <yeobi1124@gmail.com>
2025-08-07 18:11:07 +0900

    feat: Heal VFX

--- Assets/Scripts/Skill/Heal.cs
+++ Assets/Scripts/Skill/Heal.cs
@@ -1,10 +1,26 @@
 ﻿
+using System.Collections;
 using UnityEngine;
 
 public class Heal : Skill
 {
     [SerializeField] private SpaceShip spaceShip;
     [SerializeField] private float healAmount = 5f;
+
+    [SerializeField] private GameObject healParticle;
+    [SerializeField] private float vfxDuration = 2f;
+
+    private Coroutine coroutine;
+    
+    private IEnumerator ActivateVFX()
+    {
+        healParticle.SetActive(true);
+        
+        yield return new WaitForSeconds(vfxDuration);
+        
+        healParticle.SetActive(false);
+        coroutine = null;
+    }
     
     public override bool Use()
     {
@@ -13,6 +29,9 @@ public class Heal : Skill
         _energy.Energy -= _energyConsumption;
         spaceShip.health += healAmount;
 
+        if(coroutine != null) StopCoroutine(coroutine);
+        coroutine = StartCoroutine(ActivateVFX());
+        
         return true;
     }
 }
