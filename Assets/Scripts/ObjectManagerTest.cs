d67069f747902c1466e7d6bd5c14532e8af871a6
Yeobi1124 <yeobi1124@gmail.com>
2025-08-07 18:52:58 +0900

    fix: player basic attack

---
+++ Assets/Scripts/ObjectManagerTest.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class ObjectManagerTest : MonoBehaviour
+{
+    [ContextMenu("Summon Player Bullet")]
+    public void SummonPlayerBullet()
+    {
+        GameObject obj = ObjectManager.instance.PullObject("PlayerBullet");
+        obj.transform.position = transform.position;
+    }
+}
