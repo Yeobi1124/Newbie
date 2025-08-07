c7ced7273e66dfce2da2dbb59c8509a6785bd6a1
Zzalbs <kcjlazer@naver.com>
2025-08-07 20:54:26 +0900

    fix: 레이저, 보스 머티리얼 깨지는 버그 수정

--- Assets/Scripts/Boss/BT/BTInitializer.cs
+++ Assets/Scripts/Boss/BT/BTInitializer.cs
@@ -10,11 +10,9 @@ public class BTInitializer
     {
         this.behaviorTree = behaviorTree;
     }
-    public void Init(GameObject player,IMissileMover mover,Transform missileAbove, Transform missileBelow, List<GameObject> backgrounds,GameObject laser,float hp,List<GameObject> WayPoints,Transform laserTransform)
+    public void Init(GameObject player,IMissileMover mover, List<GameObject> backgrounds,GameObject laser,float hp,List<GameObject> WayPoints,Transform laserTransform)
     {
         behaviorTree.SetVariableValue("Player", player);
-        behaviorTree.SetVariableValue("MissilePosition_Above", missileAbove);
-        behaviorTree.SetVariableValue("MissilePosition_Below", missileBelow);
 		behaviorTree.SetVariableValue("Backgrounds", backgrounds);
 		behaviorTree.SetVariableValue("Laser", laser);
         behaviorTree.SetVariableValue("maxHP", hp);
